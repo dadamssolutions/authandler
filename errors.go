0ded4c513fecfea6233ee108953ca09f6b368ceb
Donnie Adams <donald.g.adams@me.com>
2018-06-22 12:34:40 -0700

    Descriptive errors

---
+++ errors.go
@@ -0,0 +1,20 @@
+package seshandler
+
+import "errors"
+
+func sessionNotFoundError(sessionID string) error {
+	return errors.New("The session ID " + sessionID + " was not found")
+}
+
+func sessionExpiredError(sessionID string) error {
+	return errors.New("The session ID " + sessionID + " is expired")
+}
+
+func databaseTableCreationError() error {
+	return errors.New("Cannot create sessions table in the database")
+}
+
+// This is only used in testing
+func databaseAccessError() error {
+	return errors.New("Cannot access database")
+}
