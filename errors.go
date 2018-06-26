fefb57c14f125fd94cd06de165a8bb585ad4e93a
Donnie Adams <donald.g.adams@me.com>
2018-06-26 11:01:49 -0700

    Use selector ID throughout

--- errors.go
+++ errors.go
@@ -13,16 +13,16 @@ func invalidSessionCookie() error {
 	return errors.New("Cookie does not represent a valid session cookie")
 }
 
-func invalidSessionError(ID string) error {
-	return errors.New("Session with ID " + ID + " is not valid")
+func invalidSessionError(selectorID string) error {
+	return errors.New("Session with selectorID " + selectorID + " is not valid")
 }
 
-func sessionExpiredError(sessionID string) error {
-	return errors.New("The session ID " + sessionID + " is expired")
+func sessionExpiredError(selectorID string) error {
+	return errors.New("The session ID " + selectorID + " is expired")
 }
 
-func sessionNotInDatabaseError(id string) error {
-	return errors.New("Session with selector ID " + id + " was not found in the database")
+func sessionNotInDatabaseError(selectorID string) error {
+	return errors.New("Session with selector ID " + selectorID + " was not found in the database")
 }
 
 func noSessionCookieFoundInRequest() error {
