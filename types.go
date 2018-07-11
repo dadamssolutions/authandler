a9bec10a7de6df2b213334486db4af3a8a2bcc89
Donnie Adams <donald.g.adams@me.com>
2018-07-10 22:56:12 -0700

    Better comment for CSRFHanlder type

--- types.go
+++ types.go
@@ -2,5 +2,8 @@ package httpauth
 
 import "net/http"
 
-// CSRFHandler is a request handler that will take a CSRF token as a string in GET requests
+// CSRFHandler is a request handler that will take a CSRF token as a string in GET requests.
+// This type of handler should be used on any page that contains a form.
+// The form should have a line that reads (or something like it):
+// `<input type="hidden" name="token" value="{{.token}}">`
 type CSRFHandler func(http.ResponseWriter, *http.Request, string, error)
